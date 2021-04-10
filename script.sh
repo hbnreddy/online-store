59cec2e4d52c0a88d9c3a9528c8b63a45d20c7fc
hbnreddy <73657652+hbnreddy@users.noreply.github.com>
2021-04-10 22:09:18 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "apt repo update"
-apt update
-echo "install nginx"
-apt install nginx -y &> output.txt
-echo "start nginx"
-service nginx start
+#echo "apt repo update"
+#apt update
+#echo "install nginx"
+#apt install nginx -y &> output.txt
+#echo "start nginx"
+#service nginx start
+apt  install tree -y
