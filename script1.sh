cb4c45e5b92634c6c77603b9e62e3ff941d55dc9
hbnreddy <73657652+hbnreddy@users.noreply.github.com>
2021-04-10 22:28:05 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#echo "apt repo update"
-#apt update
+echo "apt repo update"
+apt update
 #echo "install nginx"
 #apt install nginx -y &> output.txt
 #echo "start nginx"
 #service nginx start
-apt remove tree -y > output.txt
+apt remove tree -y &> output.txt
+cat output.txt 
