d88d675b1254ad150f485a9f589eebf912f7a194
hbnreddy <73657652+hbnreddy@users.noreply.github.com>
2021-04-10 22:33:30 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -5,5 +5,7 @@ apt update
 #apt install nginx -y &> output.txt
 #echo "start nginx"
 #service nginx start
-apt remove tree -y &> output.txt
-cat output.txt 
+echo "unmask apache2"
+systemctl unmask apache2
+echo "restart apache2"
+service apache2 restart
